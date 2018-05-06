686a28aa8a8ece139fe5f3c9f84e7bbd69413ff6
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-3.local>
2018-05-06 23:56:14 +0200

    adds data loading

--- regression.py
+++ regression.py
@@ -6,3 +6,33 @@ Created on Sun May  6 23:22:51 2018
 @author: lukaskemmer
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+##########################################################
+# Load data
+##########################################################
+data = np.load("Approx-Data.npz")
+x = data['x']
+y = data['y']
+
+##########################################################
+# b) Bestimme Minimalprunkt für P1 und P2
+##########################################################
+
+
+
+
+##########################################################
+# c) Plotte Datenpunkte sowie f1, f2
+##########################################################
+
+plt.style.use('seaborn-ticks')
+fig, ax = plt.subplots()
+ax.plot(x, y, 'o')
+plt.title("Regression")
+plt.xlabel("x")
+plt.ylabel("y")
+plt.show()
+
+
